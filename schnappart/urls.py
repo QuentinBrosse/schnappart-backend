e87a92c28bedbfbafdd99041acffaba63d179f21
Quentin Brosse <quentin.brosse@icloud.com>
2018-06-19 14:36:07 +0200

    Update auth api urls

--- schnappart/urls.py
+++ schnappart/urls.py
@@ -20,6 +20,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api-token-auth/', obtain_auth_token)
+    path('api/auth/', include('rest_framework.urls')),
+    path('api/token-auth/', obtain_auth_token)
 ]
